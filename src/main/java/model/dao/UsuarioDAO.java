4e9f461e32971a3648b05b2ffa0d967577ba502a
pivisoto <pivisoto10@gmail.com>
2022-05-20 20:26:41 -0300

    Adicionei em UsuarioDAO a função tela_ranking();

--- src/main/java/model/dao/UsuarioDAO.java
+++ src/main/java/model/dao/UsuarioDAO.java
@@ -7,7 +7,12 @@ package model.dao;
 import connection.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.bean.Usuario;
 
@@ -36,5 +41,34 @@ public class UsuarioDAO {
         } finally {
             ConnectionFactory.closeConnection(c);
         }
+        
+    }
+    public List<Usuario> listar_ranking(){
+        Connection c = ConnectionFactory.getConnection();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        
+        List<Usuario> usuarios = new ArrayList<>();
+        try{
+            st = c.prepareStatement("SELECT * FROM usuario");
+            rs = st.executeQuery();
+            
+            while (rs.next()){
+                
+                Usuario usuario = new Usuario();
+                
+                usuario.setNome(rs.getString("nome"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setPontuacao(rs.getInt("pontuacao"));
+                usuario.setStatusAdmin(rs.getBoolean("statusAdmin"));
+                usuarios.add(usuario);
+                
+            }
+        }catch(SQLException ex){
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{    
+            ConnectionFactory.closeConnection(c);
+        }
+        return usuarios;
     }
 }
